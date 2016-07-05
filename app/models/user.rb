2500a78333da2b13bb142ec927f5e92b818bb943
Chris <chris.e.ruland@gmail.com>
2016-07-05 22:26:56 +0000

    Adding file upload

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,8 @@ class User < ActiveRecord::Base
                          uniqueness: { case_sensitive: false }
      has_secure_password
      validates :password, length: { minimum: 6 }, allow_blank: true
+     has_attached_file :pic
+     has_attached_file :attach
      
      # Returns the hash digest of the given string.
      def User.digest(string)
